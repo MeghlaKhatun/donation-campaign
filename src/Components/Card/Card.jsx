b4edb3d822ee86c1d9f7913360509dd0f15a6e84
meghla khatun <meghlaparvin45@gmail.com>
2023-09-26 12:43:22 +0600

    added a some problem solution

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -10,9 +10,9 @@ const Card = ({card}) => {
     
   return (
     <div >
-        <div style={{background:card_bg}} className='rounded-lg'>
+        <div style={{background:card_bg}} className='h-[350px] rounded-lg'>
            <Link to={`/card/${id}`}>
-           <img className='w-full' src={img} alt="" />
+           <img className='w-full h-[250px] rounded-t-lg' src={img} alt="" />
             
             <h3 style={{background:category_bg, color:text_color}} className='text-sm font-medium mx-4 text-black py-1 px-3 w-1/3 rounded mt-4 text-center'>{category}</h3>
             <h2 style={{color:text_color }} className='text-xl font-semibold mx-4 pt-2 pb-4'> {title}</h2>
